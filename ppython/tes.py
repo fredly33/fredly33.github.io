e7640839d2f8a996329b6370e801105bcc2d3ee7
fredly33 <fredlyramdani@gmail.com>
2024-01-25 15:13:02 +0700

    first commit

---
+++ ppython/tes.py
@@ -0,0 +1,29 @@
+#x=['aku','kamu','dia','dia','dia','dia','dia','dia','dia','dia','dia','dia','dia','dia','dia']
+
+#print(f'index 0 ={x[0]} , index 1 = {x[1]}') buat manggil salah satu dari grup
+
+#for value in x: panggil all di grup
+
+#x = 5
+#y = 9
+#while (x<y):
+#print('Halo dunia')
+
+
+#  if (False):
+# # kondisi a
+# print('kondisi benar')
+# elif (False):
+# #kondisi b
+# print('kondisi salah')
+# else: #pengecualian terakhir
+#  print('semua kondisi salah')
+
+# x = input('masukan kata kata')
+# print(f'kata kata adalah {x}') #buat nambahain kata kata
+
+# x= (input('masukan angka 1'))
+# y= (input('masukan angka 2'))
+# hasil= x + y
+
+# print(f'{x} + {y} = ()')
